800b4346f0dbd915829da39d337f39c4a6165131
Arpit Patni <arpit@overdose.digital>
2023-11-23 00:44:19 +0530

    Practice

--- 6_Arrays3_2DArrays/1_RowWiseSum.cpp
+++ 6_Arrays3_2DArrays/1_RowWiseSum.cpp
@@ -1,31 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void printRowWiseSum(int arr[3][3],int row,int col){
-    cout<<"Row wise sum is "<<endl;
-    for(int i=0;i<row;i++){
-        int sum=0;
-        for(int j=0;j<col;j++){
-            sum=sum+arr[i][j];
+void printRowWiseSum(int arr[3][3], int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < col; j++)
+        {
+            sum = sum + arr[i][j];
         }
-        cout<<sum<<endl;
+        cout << sum << " ";
     }
 }
-int main(){
+int main()
+{
     int arr[3][3];
 
-cout<<"Enter the elments in an array"<<endl;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cin>>arr[i][j];
+    cout << "Enter the elments in an array" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> arr[i][j];
         }
-    }
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
     }
 
-    printRowWiseSum(arr,3,3);
+    printRowWiseSum(arr, 3, 3);
 }
