590ce22f2a8b1520ed8a50965f1a585b5d875c2c
Arpit Patni <arpitpatni00@gmail.com>
2024-05-09 21:52:49 +0530

    Striver Graphs

---
+++ 48_Graphs_By_Striver/4_ConvertMatrixIntoAdjList.cpp
@@ -0,0 +1,9 @@
+ vector<int>adjList[V];
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(adj[i][j]==1 && i!=j){
+                    adjList[i].push_back(j);
+                    adjList[j].push_back(i);
+                }
+            }
+        }
